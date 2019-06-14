bcf1b579e4cc829ce6169c0922d997923c9b98d2
Jolyon Russ <jolyonruss@gmail.com>
2019-06-14 07:39:18 -0400

    Sets up Jest and a snapshot test for Header

---
+++ components/Header/Header.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./Header";
+
+it("renders correctly", () => {
+  const tree = shallow(<Header />).html();
+  expect(tree).toMatchSnapshot();
+});
---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  setupFiles: ["<rootDir>/jest.setup.js"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testRegex: ".*\\.test\\.js$",
+  moduleFileExtensions: ["js", "jsx"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js"
+  },
+  collectCoverageFrom: [
+    "!jest.config.js",
+    "!jest.setup.js",
+    "!server.js",
+    "!nextjs-server.js",
+    "**/*.{js,jsx}",
+    "!pages/**",
+    "!coverage/**"
+  ]
+};

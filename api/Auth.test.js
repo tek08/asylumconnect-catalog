6049f65c6c029237875964f264b27fd568046b9a
Jolyon Russ <jolyonruss@gmail.com>
2019-06-14 07:59:46 -0400

    Adds test for Auth with mocked Axios achieving 100% test coverage 🎉

---
+++ api/Auth.test.js
@@ -0,0 +1,64 @@
+import mockAxios from "axios";
+import Auth from "./Auth";
+
+it("Auth returns a jwt when posting is successful", async () => {
+  mockAxios.post.mockImplementationOnce(() =>
+    Promise.resolve({
+      data: { jwt: "a-long-string-with-letters-and-numbers" }
+    })
+  );
+
+  const authAPI = new Auth();
+  const loginResponse = await authAPI.login({
+    body: { username: "foo@bar.baz", password: "foobarbaz" }
+  });
+
+  expect(loginResponse).toEqual({
+    jwt: "a-long-string-with-letters-and-numbers"
+  });
+  expect(mockAxios.post).toHaveBeenCalledTimes(1);
+  expect(mockAxios.post).toHaveBeenCalledWith(
+    `${process.env.ONE_DEGREE_AUTH_URL}/api/session`,
+    {
+      session: {
+        login_key: "foo@bar.baz",
+        password: "foobarbaz"
+      }
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        OneDegreeSource: "asylumconnect"
+      }
+    }
+  );
+});
+
+it("Auth returns an error when posting fails", async () => {
+  mockAxios.post.mockImplementationOnce(() =>
+    Promise.reject(new Error("Bad credentials"))
+  );
+
+  const authAPI = new Auth();
+  const loginResponse = await authAPI.login({
+    body: { username: "wrong@user.name", password: "bad-password" }
+  });
+
+  expect(loginResponse).toEqual(new Error("Bad credentials"));
+  expect(mockAxios.post).toHaveBeenCalledTimes(1);
+  expect(mockAxios.post).toHaveBeenCalledWith(
+    `${process.env.ONE_DEGREE_AUTH_URL}/api/session`,
+    {
+      session: {
+        login_key: "wrong@user.name",
+        password: "bad-password"
+      }
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        OneDegreeSource: "asylumconnect"
+      }
+    }
+  );
+});
--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js"
   },
+  clearMocks: true,
   collectCoverageFrom: [
     "!jest.config.js",
     "!jest.setup.js",
